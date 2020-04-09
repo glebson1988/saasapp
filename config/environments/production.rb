d5c0f0af13843b97b9e9a5083ae8a9071e3b3d9a
glebson1988 <glebson1988@yandex.ru>
2020-04-09 23:07:33 +0300

    add email with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "saasapp_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'glebson-saasapp.herokuapp.com', protocol: 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

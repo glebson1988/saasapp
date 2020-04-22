a8091ed7d2a93e818b5c5650281146ca5816f632
glebson1988 <glebson1988@yandex.ru>
2020-04-22 15:16:38 +0300

    add stripe and payment

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,6 +4,8 @@ class Tenant < ApplicationRecord
   acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
   has_many :projects, dependent: :destroy
+  has_one :payment
+  accepts_nested_attributes_for :payment
   validates_uniqueness_of :name
   validates_presence_of :name
 
@@ -11,38 +13,20 @@ class Tenant < ApplicationRecord
     tenant = Tenant.new(tenant_params)
 
     if new_signups_not_permitted?(coupon_params)
-
-      raise ::Milia::Control::MaxTenantExceeded, 'Sorry, new accounts not permitted at this time'
-
+      raise ::Milia::Control::MaxTenantExceeded,
+            'Sorry, new accounts not permitted at this time'
     else
-      tenant.save # create the tenant
+      tenant.save
     end
 
     tenant
   end
 
-  # ------------------------------------------------------------------------
-  # new_signups_not_permitted? -- returns true if no further signups allowed
-  # args: params from user input; might contain a special 'coupon' code
-  #       used to determine whether or not to allow another signup
-  # ------------------------------------------------------------------------
   def self.new_signups_not_permitted?(_params)
     false
   end
 
-  # ------------------------------------------------------------------------
-  # tenant_signup -- setup a new tenant in the system
-  # CALLBACK from devise RegistrationsController (milia override)
-  # AFTER user creation and current_tenant established
-  # args:
-  #   user  -- new user  obj
-  #   tenant -- new tenant obj
-  #   other  -- any other parameter string from initial request
-  # ------------------------------------------------------------------------
   def self.tenant_signup(user, _tenant, _other = nil)
-    #  StartupJob.queue_startup( tenant, user, other )
-    # any special seeding required for a new organizational tenant
-    #
     Member.create_org_admin(user)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_420_114_301) do
+ActiveRecord::Schema.define(version: 20_200_422_115_759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20_200_420_114_301) do
     t.datetime 'updated_at', null: false
     t.index ['tenant_id'], name: 'index_members_on_tenant_id'
     t.index ['user_id'], name: 'index_members_on_user_id'
+  end
+
+  create_table 'payments', force: :cascade do |t|
+    t.string 'email'
+    t.string 'token'
+    t.bigint 'tenant_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['tenant_id'], name: 'index_payments_on_tenant_id'
   end
 
   create_table 'projects', force: :cascade do |t|
@@ -94,6 +103,7 @@ ActiveRecord::Schema.define(version: 20_200_420_114_301) do
   add_foreign_key 'artifacts', 'projects'
   add_foreign_key 'members', 'tenants'
   add_foreign_key 'members', 'users'
+  add_foreign_key 'payments', 'tenants'
   add_foreign_key 'projects', 'tenants'
   add_foreign_key 'tenants', 'tenants'
 end

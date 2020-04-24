76c22a652d20b6dcbe078598c98f7669bc288bb9
glebson1988 <glebson1988@yandex.ru>
2020-04-24 23:20:21 +0300

    add admin user for organizations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Milia::RegistrationsController
 
   def create
     tenant_params = sign_up_params_tenant
-    user_params = sign_up_params_user
+    user_params = sign_up_params_user.merge(is_admin: true)
     coupon_params = sign_up_params_coupon
 
     sign_out_session!
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_422_115_759) do
+ActiveRecord::Schema.define(version: 20_200_424_201_455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20_200_422_115_759) do
     t.bigint 'tenant_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.boolean 'is_admin', default: false
     t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
